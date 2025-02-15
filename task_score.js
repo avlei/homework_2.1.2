2c480971e9f18e00f1de9b6ea92212e6f57a0aa6
Anna Labeysh <alabeish@hotmail.com>
2025-02-15 18:52:31 +0100

    student_score_added

---
+++ task_score.js
@@ -0,0 +1,21 @@
+//введем данные о баллах, полученных студентом
+let введение = 7;
+let git = 5;
+let js = 6;
+
+//Имя и фамилия студента
+let firstName = "Ivan";
+let lastName = "Ivanov"
+ 
+//подсчитаем общий балл
+let total = введение + git + js;
+
+//подсчитаем средний балл
+let AverageScore = total/3
+
+console.log(`Студент: ${firstName} ${lastName}`);
+console.log(`Общий балл: ${total}`);
+console.log(`Средний балл: ${AverageScore}`);
+
+
+ 
